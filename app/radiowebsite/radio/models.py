6b6bbab24be75a3f13c2063b283ad14eff3da165
Aleph <aleph@covinette.net>
2023-01-03 13:50:52 +0300

    Дополнены страницы, добавлен макет загрузки файлов

--- app/radiowebsite/radio/models.py
+++ app/radiowebsite/radio/models.py
@@ -1,3 +1,3 @@
 from django.db import models
 
-# Create your models here.
+# Пока что неизвестно что с этим делать
--- app/radiowebsite/radio/views.py
+++ app/radiowebsite/radio/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import AudioUploadForm
+from django.views import generic
 
-def index(request):
-    context = {}
-    return render(request, 'radio/index.html', context)
+
+class IndexView(generic.TemplateView):
+    template_name = 'radio/index.html'
+    
+
+class AudioUploadFormView(generic.FormView):
+    form_class = AudioUploadForm
+    template_name = 'radio/upload.html'
+    success_url = '/'
