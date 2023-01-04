7d2932b0509c52e55e66984b939658d0abf3b157
Aleph <aleph@covinette.net>
2023-01-04 15:09:00 +0300

    Идентификация файлов через API

--- app/radiowebsite/radio/views.py
+++ app/radiowebsite/radio/views.py
@@ -1,15 +1,25 @@
 from .models import AudioUploadForm, VideoUploadForm
 from django.views import generic
-from .Stream import readMedia
+from django.http import HttpResponseRedirect
+from .Stream import readMedia, streamQueue
 
 class IndexView(generic.TemplateView):
     template_name = 'radio/index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.GET.get('stream-start') != None:
+            print("Stream test successful")
+            return HttpResponseRedirect('/')
+        elif request.GET.get('audio-remove') != None:
+            return HttpResponseRedirect('/')
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['audio_list'] = readMedia()
         return context
-
+    
 class AudioUploadFormView(generic.FormView):
     form_class = AudioUploadForm
     template_name = 'radio/upload.html'
