e20b04e22b133097d09ed02e704e9c2d2e966ff1
Aleph <aleph@covinette.net>
2023-01-06 17:23:41 +0300

    Предварительные преобразования для видео

--- app/radiowebsite/radio/Stream.py
+++ app/radiowebsite/radio/Stream.py
@@ -1,5 +1,5 @@
 import subprocess
-from os import listdir, path, getcwd
+from os import listdir, path, getcwd, remove
 from threading import Thread
 import time
 
@@ -14,13 +14,21 @@ def getDuration(filename: str):
     output = output[output.find('duration') + len('duration') + 1: output.rfind('FORMAT') - 6]
     return int(round(float(output) + 0.5))
 
+def prepareVideo(filename: str):
+    # Небольшие преобразования видео которые лучше выполнить заранее
+    add = '-movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"'.split() if filename[0][-4:] == '.gif' else ''
+    args = ['ffmpeg', '-v', 'quiet', '-i', f'media/{filename}', *add, '-codec:v', 'libx264', '-profile:v', 'high',
+            '-preset', 'veryslow', '-b:v', '2500k', '-maxrate', '2500k', '-bufsize', '5000k', '-threads', '0', f'media/cached/{filename}']
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+    popen.wait()
+
+
 def streamFragment(afilename: (str, int), vfilename: (str, int), secret: str):
     # Совмещает видео с музыкой и стримит на Ютуб
-    add = '-movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"'.split() if vfilename[0][-4:] == '.gif' else ''
     vdur, adur = vfilename[1], afilename[1]
-    args = ['ffmpeg', '-stream_loop', f'{adur // vdur}', '-re'
-            '-i', f'media/{vfilename[0]}', *add, '-i', f'media/{afilename[0]}', '-af', 
-            f'afade=t=in:st=0:d=3,afade=t=out:st={vdur * (adur // vdur) - 3}:d=3', '-codec:v', 'libx264', '-profile:v', 'high', '-preset', 'slow', 
+    args = ['ffmpeg', '-v', 'quiet', '-stream_loop', f'{adur // vdur}', '-re',
+            '-i', f'media/cached/{vfilename[0]}', '-i', f'media/{afilename[0]}', '-af', 
+            f'afade=t=in:st=0:d=3,afade=t=out:st={vdur * (adur // vdur) - 3}:d=3', '-codec:v', 'libx264',
             '-ss', '0', '-t', f'{vdur * (adur // vdur)}',
             '-f', 'flv', 'rtmp://a.rtmp.youtube.com/live2/' + secret]
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -28,6 +36,13 @@ def streamFragment(afilename: (str, int), vfilename: (str, int), secret: str):
 
 def streamQueue(aqueue: list, vqueue: list, secret: str):
     # Поочереди (асинхронно) стримит всю музыку
+    print(f'[XI] Производится предварительное транскодирование, подождите...')
+    for video in vqueue:
+        try:
+            remove(getcwd() + '/media/cached/' + video[0])
+        except FileNotFoundError:
+            pass
+        prepareVideo(video[0])
     print(f'[XI] Стрим запущен успешно, ключ: {secret}')
     sstream = Thread(target=streamFragment, args=(aqueue[0], vqueue[0], secret))
     sstream.start()
@@ -39,3 +54,6 @@ def streamQueue(aqueue: list, vqueue: list, secret: str):
         nstream.start()
         print(f"[XI] Номер проигрываемого трека: {afile + 1} ({aqueue[afile][0]})")
         delay = vqueue[0][1] * (aqueue[afile][1] // vqueue[0][1])
+    nstream.join()
+    for video in vqueue:
+        remove(getcwd() + '/media/cached/' + video[0])
